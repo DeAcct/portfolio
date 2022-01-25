3f423d345637a5a24a918233f205dfa871c9152f
Hyeon Seong LEE <dynamic1117@gmail.com>
2022-01-25 21:34:53 +0900

    complete styling contact component

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 import "./style/style.scss";
+
+const $main = document.querySelector("main")!;
+const $header = document.querySelector(".header")!;
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > $main.offsetTop) {
+    $header.classList.add("header--filled");
+  } else {
+    $header.classList.remove("header--filled");
+  }
+});
+
+const $naviOpenBtn = document.querySelector(".navi-open-btn")!;
+const $navi = document.querySelector(".navi")!;
+$naviOpenBtn.addEventListener("click", () => {
+  $navi.classList.toggle("navi--opened");
+});
